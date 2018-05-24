d64c92400e9a4088767ccaab263cafc379e97031
incomein <2592491707@qq.com>
2018-05-24 17:24:13 +0800

    Initial commit

---
+++ src/main/java/com/test/test.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public test(String name) {
+        this.name = name;
+    }
+    public test() {
+
+    }
+
+}
